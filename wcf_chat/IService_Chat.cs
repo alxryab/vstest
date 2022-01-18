2f843763df73fe3987779b5728baaf561640da05
vstest <vs@mailmail.com>
2022-01-18 15:22:05 +0300

    Init commit

---
+++ wcf_chat/IService_Chat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_chat
+{
+    // ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "IService_Chat" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IService_Chat
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
---
+++ wcf_chat/Service_Chat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_chat
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service_Chat" в коде и файле конфигурации.
+    public class Service_Chat : IService_Chat
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
